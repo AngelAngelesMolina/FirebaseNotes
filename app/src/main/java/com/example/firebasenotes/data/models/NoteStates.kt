01ba90ab6cd543eeece9882a4b0a01e395942f1e
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-05-20 21:14:13 -0600

    CardList from fb

---
+++ app/src/main/java/com/example/firebasenotes/data/models/NoteStates.kt
@@ -0,0 +1,9 @@
+package com.example.firebasenotes.data.models
+
+data class NoteStates(
+    val emailUser: String = "",
+    val title: String = "",
+    val note: String = "",
+    val date: String = "",
+    val idDoc: String = ""
+)
--- app/src/main/java/com/example/firebasenotes/presentation/viewmodels/NotesViewModel.kt
+++ app/src/main/java/com/example/firebasenotes/presentation/viewmodels/NotesViewModel.kt
@@ -3,12 +3,16 @@ package com.example.firebasenotes.presentation.viewmodels
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.firebasenotes.data.models.NoteStates
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObject
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Calendar
@@ -17,8 +21,28 @@ import java.util.Locale
 
 class NotesViewModel : ViewModel() {
     private val auth: FirebaseAuth = Firebase.auth
-
     private val firestore: FirebaseFirestore = Firebase.firestore
+
+    private val _notesData = MutableStateFlow<List<NoteStates>>(emptyList())
+    val notesData: StateFlow<List<NoteStates>> = _notesData
+
+    fun fetchNotes() {
+        val email = auth.currentUser?.email
+
+        firestore.collection("Notes").whereEqualTo("emailUser", email)
+            .addSnapshotListener { querySnapshot, error ->
+                if (error != null) {
+                    return@addSnapshotListener
+                }
+                val documents = mutableListOf<NoteStates>()
+                querySnapshot?.forEach { document ->
+                    val document = document.toObject(NoteStates::class.java)
+                        .copy(idDoc = document.id) //Se saca todo el documento
+                    documents.add(document)
+                }
+                _notesData.value = documents
+            }
+    }
 
     fun saveNewNote(title: String, note: String, onSuccess: () -> Unit) {
         val email = auth.currentUser?.email
