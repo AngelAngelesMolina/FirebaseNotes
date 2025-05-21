64979de76940e8d835a94d4ed16ef275a232730f
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-05-20 21:14:24 -0600

    CardNote

--- app/src/main/java/com/example/firebasenotes/presentation/views/notes/HomeView.kt
+++ app/src/main/java/com/example/firebasenotes/presentation/views/notes/HomeView.kt
@@ -3,6 +3,8 @@ package com.example.firebasenotes.presentation.views.notes
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.filled.Add
@@ -13,20 +15,27 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.example.firebasenotes.presentation.components.CardNote
 import com.example.firebasenotes.presentation.viewmodels.NotesViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeView(navController: NavController, notesViewModel: NotesViewModel, pad: PaddingValues) {
-
+    LaunchedEffect(Unit) {
+        notesViewModel.fetchNotes()
+    }
     Scaffold(
         topBar = {
-            TopAppBar(title = {
-                Text(text = "Mis notas")
-            },
+            TopAppBar(
+                title = {
+                    Text(text = "Mis notas")
+                },
                 navigationIcon = {
                     IconButton(onClick = {
                         notesViewModel.logOut()
@@ -50,10 +59,15 @@ fun HomeView(navController: NavController, notesViewModel: NotesViewModel, pad: 
     ) { pad ->
         Column(
             modifier = Modifier
-//                .fillMaxSize()
                 .padding(pad), horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "HomeView")
+            val notesData by notesViewModel.notesData.collectAsState()
+            LazyColumn {
+                items(notesData) { note ->
+                    CardNote(title = note.title, note = note.note, date = note.date) {
+                    }
+                }
+            }
         }
     }
 }
